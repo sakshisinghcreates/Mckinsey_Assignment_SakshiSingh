79041ee7c3507163c0b142d4e5edb49cf7318cd9
Sakshi Singh <sakshisingh0820@gmail.com>
2024-08-09 20:04:22 +0530

    Added Login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username = '';
+  password = '';
 
+  constructor(private apiService: ApiService) {}
+
+  login() {
+    this.apiService.login({ username: this.username, password: this.password }).subscribe(response => {
+      console.log(response);
+      // Handle login success
+    }, error => {
+      console.error(error);
+      // Handle login error
+    });
+  }
 }

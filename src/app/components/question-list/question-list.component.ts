f249c8aab36a82ad7393fa6520b3586e7e9ba46f
Sakshi Singh <sakshisingh0820@gmail.com>
2024-08-09 20:05:06 +0530

    Added Question List component

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-question-list',
-  standalone: true,
-  imports: [],
   templateUrl: './question-list.component.html',
-  styleUrl: './question-list.component.scss'
+  styleUrls: ['./question-list.component.css']
 })
-export class QuestionListComponent {
+export class QuestionListComponent implements OnInit {
+  questions: any[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getQuestions().subscribe(response => {
+      this.questions = response;
+    }, error => {
+      console.error(error);
+    });
+  }
 }

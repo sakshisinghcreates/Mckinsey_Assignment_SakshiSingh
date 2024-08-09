e9a441888f4c731e6cbc4019a986a328ef3dabdb
Sakshi Singh <sakshisingh0820@gmail.com>
2024-08-09 20:45:13 +0530

    Fixed routing and added missing stylesheets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { QuestionListComponent } from './components/question-list/question-list.component';
+import { CandidateListComponent } from './components/candidate-list/candidate-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'questions', component: QuestionListComponent },
+  { path: 'candidates', component: CandidateListComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
+
+

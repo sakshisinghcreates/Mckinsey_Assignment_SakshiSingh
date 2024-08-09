dd7627d35f8b38dbec396dcc2261d0de62ed04e0
Sakshi Singh <sakshisingh0820@gmail.com>
2024-08-09 19:55:37 +0530

    Set up Angular project with Angular Material, and created initial components and services

---
+++ src/app/components/question-detail/question-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './question-detail.component.html',
+  styleUrl: './question-detail.component.scss'
+})
+export class QuestionDetailComponent {
+
+}
